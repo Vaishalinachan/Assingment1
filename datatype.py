22a9539b83d21f9550fa9f21944ddb98fa58a8cb
Vaishalinachan <156295195+Vaishalinachan@users.noreply.github.com>
2024-01-11 13:37:11 +0530

    Question1

---
+++ datatype.py
@@ -0,0 +1,36 @@
+#List
+# are used to stored more than one value
+# index start with 0
+# lst=[2,5,6,7,9]
+# print(lst)
+# print(lst[3])
+
+# lst=[2,5,6,4,7]
+# lst[0]="Dipak"
+# print(lst)
+
+# print(type(lst))
+
+# lst1=list([1,2,3,4])
+# print(lst1)
+# print(lst1[1])
+
+
+#tuple
+#tup=(1,2,70,10,25)
+#print(tup)
+
+# tup1=tuple((1,2,3))
+# print(tup1)
+
+# print(tup[0])
+
+#Unique list from the given list
+lst=[1,2,3,4,5,6,6,7,1]
+print(lst)
+
+lst1=set(lst)
+print(lst1)
+
+lst2=list(lst1)
+print(lst2)
